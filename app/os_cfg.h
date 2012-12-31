9d3a826e2a990b9c0b910353a4b6f9188bea7d90
qujianning <qujianning@kyland.com.cn>
2012-12-31 09:42:06 +0800

    OS task and events define grow up

--- app/os_cfg.h
+++ app/os_cfg.h
@@ -36,15 +36,15 @@
 #define OS_EVENT_MULTI_EN         0u   /* Include code for OSEventPendMulti()                          */
 #define OS_EVENT_NAME_EN          0u   /* Enable names for Sem, Mutex, Mbox and Q                      */
 
-#define OS_LOWEST_PRIO           15u   /* Defines the lowest priority that can be assigned ...         */
+#define OS_LOWEST_PRIO           20u   /* Defines the lowest priority that can be assigned ...         */
                                        /* ... MUST NEVER be higher than 254!                           */
 #define OS_TASK_TMR_PRIO          2u   /* define timer task priority qujianning@2012/1/3               */
 
-#define OS_MAX_EVENTS            10u   /* Max. number of event control blocks in your application      */
-#define OS_MAX_FLAGS              5u   /* Max. number of Event Flag Groups    in your application      */
-#define OS_MAX_MEM_PART           5u   /* Max. number of memory partitions                             */
-#define OS_MAX_QS                 4u   /* Max. number of queue control blocks in your application      */
-#define OS_MAX_TASKS             10u   /* Max. number of tasks in your application, MUST be >= 2       */
+#define OS_MAX_EVENTS            16u   /* Max. number of event control blocks in your application      */
+#define OS_MAX_FLAGS              8u   /* Max. number of Event Flag Groups    in your application      */
+#define OS_MAX_MEM_PART           8u   /* Max. number of memory partitions                             */
+#define OS_MAX_QS                 8u   /* Max. number of queue control blocks in your application      */
+#define OS_MAX_TASKS             16u   /* Max. number of tasks in your application, MUST be >= 2       */
 
 #define OS_SCHED_LOCK_EN          1u   /* Include code for OSSchedLock() and OSSchedUnlock()           */
 
